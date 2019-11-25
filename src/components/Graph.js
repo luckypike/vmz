c251c9ea9139600fd33b6653d221787a1ebefac0
Maxim <max.spiridonov@gmail.com>
2019-11-25 18:37:40 +0300

    Переделан последний график

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,95 +1,121 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classNames from 'classnames'
+import CountUp from 'react-countup'
+import VisibilitySensor from 'react-visibility-sensor';
 
 import fonts from '../Fonts.module.css'
 import styles from './Graph.module.css'
 
-export default function Schedule () {
+export default function Graph () {
+
+  const [visible, setVisible] = useState(false)
+
+  const onVisibilityChange = isVisible => {
+    if (isVisible) {
+      setVisible(true)
+    }
+  }
+
   return(
     <div className={styles.root}>
       <div className={styles.schedule}>
-        <div className={classNames(styles.year, fonts.h4)}>
+        <div className={classNames(styles.year, fonts.small)}>
           2006 – 2008
         </div>
 
-        <div className={styles.graph}>
-          <div className={classNames(styles.indicator, styles.first)} />
-          <div className={classNames(styles.sum, fonts.h4)}>
-            80 млн руб.
-          </div>
-        </div>
-
-        <div className={classNames(styles.description, fonts.small)}>
+        <div className={classNames(styles.description, fonts.h5)}>
           Термообезвреживание отходов смазочно-охлаждающей жидкости
         </div>
+
+        <VisibilitySensor delayedCall>
+          <div className={styles.indicators}>
+            <div className={classNames(styles.sum, fonts.small, {[styles.animated]: visible })}>
+              <CountUp start={0} end={visible ? 80 : 0} duration={2} delay={1} suffix=" млрд руб." />
+            </div>
+
+            <div className={classNames(styles.indicator, {[styles.first]: visible}, {[styles.animated]: visible })} />
+          </div>
+        </VisibilitySensor>
       </div>
 
       <div className={styles.schedule}>
-        <div className={classNames(styles.year, fonts.h4)}>
+        <div className={classNames(styles.year, fonts.small)}>
           2009 – 2015
         </div>
 
-        <div className={styles.graph}>
-          <div className={classNames(styles.indicator, styles.second)} />
-          <div className={classNames(styles.sum, fonts.h4)}>
-            244,5 млн руб.
-          </div>
-        </div>
-
-        <div className={classNames(styles.description, fonts.small)}>
+        <div className={classNames(styles.description, fonts.h5)}>
           Строительство очистных сооружений
         </div>
+
+        <VisibilitySensor delayedCall>
+          <div className={styles.indicators}>
+            <div className={classNames(styles.sum, fonts.small, {[styles.animated]: visible })}>
+              <CountUp decimals={1} start={0} end={visible ? 244.5 : 0} duration={2} delay={1} suffix=" млрд руб." />
+            </div>
+
+            <div className={classNames(styles.indicator, {[styles.second]: visible}, {[styles.animated]: visible })} />
+          </div>
+        </VisibilitySensor>
       </div>
 
       <div className={styles.schedule}>
-        <div className={classNames(styles.year, fonts.h4)}>
+        <div className={classNames(styles.year, fonts.small)}>
           2010 – 2012
         </div>
 
-        <div className={styles.graph}>
-          <div className={classNames(styles.indicator, styles.third)} />
-          <div className={classNames(styles.sum, fonts.h4)}>
-            199 млн руб.
-          </div>
-        </div>
-
-        <div className={classNames(styles.description, fonts.small)}>
+        <div className={classNames(styles.description, fonts.h5)}>
           Участок брикетирования стружки черных металлов
         </div>
+
+        <VisibilitySensor delayedCall>
+          <div className={styles.indicators}>
+            <div className={classNames(styles.sum, fonts.small, {[styles.animated]: visible })}>
+              <CountUp start={0} end={visible ? 199 : 0} duration={2} delay={1} suffix=" млрд руб." />
+            </div>
+
+            <div className={classNames(styles.indicator, {[styles.third]: visible}, {[styles.animated]: visible })} />
+          </div>
+        </VisibilitySensor>
       </div>
 
       <div className={styles.schedule}>
-        <div className={classNames(styles.year, fonts.h4)}>
+        <div className={classNames(styles.year, fonts.small)}>
           2017 – 2018
         </div>
 
-        <div className={styles.graph}>
-          <div className={classNames(styles.indicator, styles.fourth)} />
-          <div className={classNames(styles.sum, fonts.h4)}>
-            35 млн руб.
-          </div>
-        </div>
-
-        <div className={classNames(styles.description, fonts.small)}>
+        <div className={classNames(styles.description, fonts.h5)}>
           Сокращение выбросов загрязняющих веществ
         </div>
+
+        <VisibilitySensor delayedCall onChange={onVisibilityChange} offset={{ top: 50 }}>
+          <div className={styles.indicators}>
+            <div className={classNames(styles.sum, fonts.small, {[styles.animated]: visible })}>
+              <CountUp start={0} end={visible ? 35 : 0} duration={2} delay={1} suffix=" млрд руб." />
+            </div>
+
+            <div className={classNames(styles.indicator, {[styles.fourth]: visible}, {[styles.animated]: visible })} />
+          </div>
+        </VisibilitySensor>
       </div>
 
       <div className={styles.schedule}>
-        <div className={classNames(styles.year, fonts.h4)}>
+        <div className={classNames(styles.year, fonts.small)}>
           2018
         </div>
 
-        <div className={styles.graph}>
-          <div className={classNames(styles.indicator, styles.fifth)} />
-          <div className={classNames(styles.sum, fonts.h4)}>
-            13 млн руб.
-          </div>
-        </div>
-
-        <div className={classNames(styles.description, fonts.small)}>
+        <div className={classNames(styles.description, fonts.h5)}>
           Строительство шумозащитного экрана
         </div>
+
+        <VisibilitySensor delayedCall>
+          <div className={styles.indicators}>
+            <div className={classNames(styles.sum, fonts.small, {[styles.animated]: visible })}>
+              <CountUp start={0} end={visible ? 13 : 0} duration={2} delay={1} suffix=" млрд руб." />
+            </div>
+
+            <div className={classNames(styles.indicator, {[styles.fifth]: visible}, {[styles.animated]: visible })} />
+          </div>
+        </VisibilitySensor>
       </div>
     </div>
   )
